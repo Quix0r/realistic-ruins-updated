76bc1b286df04424eff03cb834115986dc52500c
Roland Häder <roland@mxchange.org>
2025-07-21 19:19:32 +0200

    Features: - added German locale Bugfixes: - updating configuration over `remote.call()` didn't work as expected. The mod was never added   as Factorio seem to not update its mod settings GUI and there is no `GUI.refresh()` or so - removed redundant (and wrong) check on if `name` is an active mod - use "register_ruin_set" instead of hard-coded settings update (Krastorio2 ruins)

---
+++ settings.lua
@@ -0,0 +1,8 @@
+-- This mod's ruin set name
+local RUN_SET_NAME = "realistic"
+
+-- Get core's utilities
+local utils = require("__AbandonedRuins_updated_fork__/lua/utilities"
+
+-- Adds this ruin set's name to the selection box
+utils.add_ruin_set(RUN_SET_NAME, true)
