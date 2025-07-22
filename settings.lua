b7e68d95727182d1d370416875df06e9d822f5d6
Roland Häder <roland@mxchange.org>
2025-07-22 10:33:24 +0200

    Bugfixes: - fixed crash (missing closing brace) - `register_ruin_set` is the proper name

--- settings.lua
+++ settings.lua
@@ -2,7 +2,7 @@
 local RUN_SET_NAME = "realistic"
 
 -- Get core's utilities
-local utils = require("__AbandonedRuins_updated_fork__/lua/utilities"
+local utils = require("__AbandonedRuins_updated_fork__/lua/utilities")
 
 -- Adds this ruin set's name to the selection box
-utils.add_ruin_set(RUN_SET_NAME, true)
+utils.register_ruin_set(RUN_SET_NAME, true)
